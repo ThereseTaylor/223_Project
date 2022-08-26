8df5966804b712aba28876c1d66ff8924ba0a74c
ThereseTaylor <112098539+ThereseTaylor@users.noreply.github.com>
2022-08-26 13:22:10 +0200

    MenuStrip added

--- Pukki_Rental/Pukki_Rental/Form1.cs
+++ Pukki_Rental/Pukki_Rental/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Pukki_Rental
 {
-    public partial class Form1 : Form
+    public partial class frmWelcome : Form
     {
-        public Form1()
+        public frmWelcome()
         {
             InitializeComponent();
         }

e8bb6bf8ea606c2370f256c02a01cee5cfb885a2
ThereseTaylor <112098539+ThereseTaylor@users.noreply.github.com>
2022-08-31 16:55:07 +0200

    Sql Schema

---
+++ Tabels.sql
@@ -0,0 +1,50 @@
+USE dbPUKKI_RENTAL
+GO
+
+CREATE TABLE VEHICLE_COLOUR(
+    ColourID int IDENTITY(1,1) PRIMARY KEY,
+    Colour_Name varchar(10)
+);
+
+CREATE TABLE VEHICLE(
+    VehicleID int IDENTITY(1,1) PRIMARY KEY,
+    TypeID int FOREIGN KEY REFERENCES VEHICLE_TYPE(TypeID),
+    ModelID int FOREIGN KEY REFERENCES VEHICLE_MODEL(ModelID),
+    ColourID int FOREIGN KEY REFERENCES VEHICLE_COLOUR(ColourID),
+    Registration_Plate varchar(10),
+    Purchase_Price money,
+    Purchase_Date date,
+    Rental_Price money
+);
+
+CREATE TABLE TOWN(
+   TownID int IDENTITY(1,1) PRIMARY KEY,
+   Town_Name varchar(35)
+);
+
+CREATE TABLE ADDRESS(
+    AddressID int IDENTITY(1,1) PRIMARY KEY,
+    TownID int FOREIGN KEY REFERENCES TOWN(TownID),
+    Street_Number varchar(5),
+    Street_Name varchar(35)
+);
+
+CREATE TABLE CLIENT(
+    ClientID int IDENTITY(1,1) PRIMARY KEY,
+    ClientLN varchar(35),
+    ClientFN varchar(35),
+    ClientID_Number char(13),
+    Tel_Number varchar(10),
+    Email varchar(35),
+    AddressID int FOREIGN KEY REFERENCES ADDRESS(AddressID),
+);
+
+CREATE TABLE RENTAL_TRANSACTION(
+    TransactionID int IDENTITY(1,1) PRIMARY KEY,
+    VehicleID int FOREIGN KEY REFERENCES VEHICLE(VehicleID),
+    ClientID int FOREIGN KEY REFERENCES CLIENT(ClientID),
+    Transaction_Price money,
+    Transaction_Date date,
+    Return_Date date,
+    Return_Status char(1)
+);

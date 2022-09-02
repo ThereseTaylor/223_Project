9a23a29f85eb80284dfcbd4e48387b60c05d61a9
LiamBrinkmann <liambrinkmann101@gmail.com>
2022-09-02 10:55:00 +0200

    commit 2
    Yes

--- Pukki_Rental/Pukki_Rental/frmVehicleInfo.Designer.cs
+++ Pukki_Rental/Pukki_Rental/frmVehicleInfo.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmVehicleInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmVehicleInfo";
+            this.ControlBox = false;
+            this.Name = "frmVehicleInfo";
+            this.ResumeLayout(false);
+
         }
 
         #endregion

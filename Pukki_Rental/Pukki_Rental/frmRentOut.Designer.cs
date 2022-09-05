7bc784dcdc1f462e80a81bbb7a6c0889c817d6be
ThereseTaylor <112098539+ThereseTaylor@users.noreply.github.com>
2022-09-05 15:32:21 +0200

    Rent out changes

--- Pukki_Rental/Pukki_Rental/frmRentOut.Designer.cs
+++ Pukki_Rental/Pukki_Rental/frmRentOut.Designer.cs
@@ -29,35 +29,112 @@
         private void InitializeComponent()
         {
             this.dgRentOut = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbxType = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dgVehicle = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgRentOut)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgVehicle)).BeginInit();
             this.SuspendLayout();
             // 
             // dgRentOut
             // 
+            this.dgRentOut.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.dgRentOut.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgRentOut.Location = new System.Drawing.Point(87, 145);
+            this.dgRentOut.ColumnHeadersVisible = false;
+            this.dgRentOut.Location = new System.Drawing.Point(52, 120);
             this.dgRentOut.Name = "dgRentOut";
-            this.dgRentOut.Size = new System.Drawing.Size(240, 150);
+            this.dgRentOut.Size = new System.Drawing.Size(823, 313);
             this.dgRentOut.TabIndex = 0;
+            this.dgRentOut.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgRentOut_CellDoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Double click on the client that is renting:";
+            // 
+            // cmbxType
+            // 
+            this.cmbxType.FormattingEnabled = true;
+            this.cmbxType.Location = new System.Drawing.Point(248, 52);
+            this.cmbxType.Name = "cmbxType";
+            this.cmbxType.Size = new System.Drawing.Size(121, 21);
+            this.cmbxType.TabIndex = 4;
+            this.cmbxType.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // dgVehicle
+            // 
+            this.dgVehicle.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgVehicle.Location = new System.Drawing.Point(52, 120);
+            this.dgVehicle.Name = "dgVehicle";
+            this.dgVehicle.Size = new System.Drawing.Size(504, 313);
+            this.dgVehicle.TabIndex = 7;
+            this.dgVehicle.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgVehicle_CellDoubleClick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(53, 457);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(223, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "How long will the client be renting the vehicle:";
             // 
             // frmRentOut
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1054, 559);
             this.ControlBox = false;
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dgVehicle);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbxType);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dgRentOut);
             this.Name = "frmRentOut";
             this.ShowIcon = false;
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.frmRentOut_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgRentOut)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgVehicle)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dgRentOut;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbxType;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dgVehicle;
+        private System.Windows.Forms.Label label4;
     }
 }
--- Pukki_Rental/Pukki_Rental/frmRentOut.cs
+++ Pukki_Rental/Pukki_Rental/frmRentOut.cs
@@ -23,6 +23,7 @@ namespace Pukki_Rental
         SqlCommand cmd;
         SqlDataAdapter adap;
         DataSet ds;
+        SqlDataReader reader;
 
 
         private void frmRentOut_Load(object sender, EventArgs e)
@@ -35,8 +36,9 @@ namespace Pukki_Rental
             ds = new DataSet();
             adap = new SqlDataAdapter();
 
-            sql = "SELECT * FROM dbo.CLIENT";
-
+            sql = "SELECT ClientLN, ClientFN, ClientID_Number, Tel_Number, Email, Street_Number, Street_Name " +
+                  "FROM dbo.CLIENT C, dbo.ADDRESS A WHERE C.AddressID = A.AddressID ";
+            
             cmd = new SqlCommand(sql, conn);
             adap.SelectCommand = cmd;
             adap.Fill(ds, "SourceTable");
@@ -45,8 +47,159 @@ namespace Pukki_Rental
             dgRentOut.DataMember = "SourceTable";
 
             conn.Close();
+
+            cmbxType.Hide();
+            label3.Text = "";
+            dgVehicle.Hide();
+            label4.Text = "";
         }
 
-     
+        int clientID;
+        int carID;
+        string rentalR;
+
+        private void dgRentOut_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            string clientPrompt = dgRentOut.Rows[e.RowIndex].Cells[2].Value.ToString() + " " + dgRentOut.Rows[e.RowIndex].Cells[1].Value.ToString() + " ID: " + dgRentOut.Rows[e.RowIndex].Cells[3].Value.ToString();
+            string sql = "";
+
+            conn.Open();
+            sql = "SELECT ClientID, Return_Status " +
+                "FROM dbo.RENTAL_TRANSACTION " +
+                "WHERE Return_Status = 'N' OR Return_Status = 'O' ";
+            cmd = new SqlCommand(sql, conn);
+            reader = cmd.ExecuteReader();
+            Boolean myflag = false;
+
+            while (reader.Read())
+            {
+                if (reader.GetValue(0).ToString() == clientID.ToString())
+                {
+                    DialogResult dialog = MessageBox.Show("This client is currently renting another car.", "CLIENT", MessageBoxButtons.OK);
+                    myflag = true;
+                }
+            }
+
+            conn.Close();
+
+            if (!myflag)
+            {
+                string title = "Are you sure " + clientPrompt + " is the correct client?";
+
+                DialogResult dialogResult = MessageBox.Show(title, "CLIENT", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    conn.Open();
+                    string idNo = dgRentOut.Rows[e.RowIndex].Cells[2].Value.ToString();
+                    sql = "SELECT ClientID FROM dbo.CLIENT WHERE ClientID_Number = " + idNo;
+                    cmd = new SqlCommand(sql, conn);
+                    reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        clientID = (int)reader.GetValue(0);
+                    }
+
+                    conn.Close();
+
+                    dgRentOut.DataSource = null;
+                    dgRentOut.Hide();
+
+                    label1.Text = "Double click on the vehicle the client wants to rent:";
+                    label3.Text = "Choose the clients preferred vehicle type:";
+
+                    cmbxType.Show();
+                    conn.Open();
+                    sql = "SELECT Type_Description FROM dbo.VEHICLE_TYPE";
+                    cmd = new SqlCommand(sql, conn);
+                    reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        cmbxType.Items.Add(reader.GetValue(0));
+                    }
+
+                    conn.Close();
+
+                }
+                else if (dialogResult == DialogResult.No)
+                {
+                    MessageBox.Show("Then please choose the correct client.", "CLIENT", MessageBoxButtons.OK);
+                }
+
+            }
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dgVehicle.Show();
+            string sql = "";
+
+            conn = new SqlConnection(conStr);
+            conn.Open();
+
+            ds = new DataSet();
+            adap = new SqlDataAdapter();
+
+            sql = "SELECT Model_Description, Type_Description, Colour_Name, Registration_Plate, Rental_Price " +
+                "FROM dbo.VEHICLE V, dbo.VEHICLE_MODEL M, dbo.VEHICLE_TYPE T, dbo.VEHICLE_COLOUR C " +
+                "WHERE V.ModelID = M.ModelID AND V.TypeID = T.TypeID AND V.ColourID = C.ColourID AND Rental_Status = 0";
+
+            cmd = new SqlCommand(sql, conn);
+            adap.SelectCommand = cmd;
+            adap.Fill(ds, "SourceTable");
+
+            dgVehicle.DataSource = ds;
+            dgVehicle.DataMember = "SourceTable";
+
+            conn.Close();
+        }
+
+        private void dgVehicle_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            string carPrompt = dgVehicle.Rows[e.RowIndex].Cells[0].Value.ToString() + " " + dgVehicle.Rows[e.RowIndex].Cells[2].Value.ToString() + " " + dgVehicle.Rows[e.RowIndex].Cells[1].Value.ToString() + " Registration Plate: " + dgVehicle.Rows[e.RowIndex].Cells[3].Value.ToString();
+            string car = "Are you sure " + carPrompt + " is the correct vehicle?";
+            
+            DialogResult Result = MessageBox.Show(car, "VEHICLE", MessageBoxButtons.YesNo);
+            if (Result == DialogResult.Yes)
+            {
+                conn.Open();
+                string regPlate = dgVehicle.Rows[e.RowIndex].Cells[3].Value.ToString();
+                string sql = "SELECT VehicleID, Rental_Price FROM dbo.VEHICLE WHERE Registration_Plate = '"+ regPlate +"'" ;
+                cmd = new SqlCommand(sql, conn);
+                reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    carID = (int)reader.GetValue(0);
+                    rentalR = reader.GetValue(1).ToString();
+                }
+                MessageBox.Show("Thank you.", "VEHICLE", MessageBoxButtons.OK);
+                conn.Close();
+
+                label4.Text = "How long will the client be renting the vehicle?";
+                DateTime today = DateTime.Today;
+
+                try
+                {
+                    conn.Open();
+                    sql = $"INSERT INTO dbo.RENTAL_TRANSACTION (VehicleID, ClientID, Transaction_Price, Transaction_Date, Return_Status) VALUES ({carID},{clientID},{rentalR},'{today}','{"N"}')";
+                    adap.InsertCommand = cmd;
+                    adap.InsertCommand.ExecuteNonQuery();
+                    conn.Close();
+                }
+                catch (Exception message)
+                {
+                    MessageBox.Show(message.ToString());
+                }
+                
+            }
+            else if (Result == DialogResult.No)
+            {
+                MessageBox.Show("Then please choose the correct vehicle.", "VEHICLE", MessageBoxButtons.OK);
+            }
+
+        }
     }
 }

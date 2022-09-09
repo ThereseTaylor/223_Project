79e36fb1078a9c7e704cd0b4e4a6d3821865fa80
ThereseTaylor <112098539+ThereseTaylor@users.noreply.github.com>
2022-09-09 15:17:52 +0200

    colour popup form

---
+++ Pukki_Rental/Pukki_Rental/frmChangePopup.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Pukki_Rental
+{
+    public partial class frmChangePopup : Form
+    {
+        public int changeColourID, whatToChange;
+        public string changeRegistration;
+        public double changeRentalCost;
+        public bool makeChange = false;
+        string conStr = @"Data Source=DESKTOP-GVIQ2PC;Initial Catalog=dbPUKKI_RENTAL;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        SqlConnection conn;
+        SqlCommand cmd;
+        SqlDataAdapter adap;
+        DataSet ds;
+        SqlDataReader reader;
+        public frmChangePopup()
+        {
+            InitializeComponent();
+        }
+
+        private void frmChangePopup_Load(object sender, EventArgs e)
+        {
+            conn = new SqlConnection(conStr);
+            conn.Open();
+            string sql = "SELECT Colour_Name FROM dbo.VEHICLE_COLOUR";
+            ds = new DataSet();
+            adap = new SqlDataAdapter();
+            cmd = new SqlCommand(sql, conn);
+            reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                cmbColour.Items.Add(reader.GetString(0));
+            }
+            conn.Close();
+
+            whatToChange = frmVehicleInfo.whatToChange;
+
+            if (whatToChange == 0)
+            {
+                lblText.Text = "Select a new colour below";
+                cmbColour.Visible = true;
+            }
+            else if (whatToChange == 1)
+            {
+                lblText.Text = "Enter the new registration below";
+                txtChangedInfo.Visible = true;
+            }
+            else if(whatToChange == 2)
+            {
+                lblText.Text = "Enter the new rental cost below";
+                txtChangedInfo.Visible = true;
+            }
+        }
+
+        private void cmbColour_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string newColour = cmbColour.GetItemText(cmbColour.SelectedItem);
+            conn.Open();
+            string sql = "SELECT * FROM dbo.VEHICLE_COLOUR";
+            cmd = new SqlCommand(sql, conn);
+            reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                if (reader.GetValue(1).ToString() == newColour)
+                {
+                    changeColourID = (int)(reader.GetValue(0));
+                }
+            }
+            conn.Close();
+            makeChange = true;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            makeChange = false;
+            this.Close();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (whatToChange == 1) //registration part
+            { 
+                if(txtChangedInfo.Text == "")
+                {
+                    MessageBox.Show("Please enter a new registration or press cancel");
+                }
+                else
+                {
+                    conn = new SqlConnection(conStr);
+                    conn.Open();
+                    string sql = "SELECT Registration_Plate FROM dbo.VEHICLE";
+                    ds = new DataSet();
+                    adap = new SqlDataAdapter();
+                    cmd = new SqlCommand(sql, conn);
+                    reader = cmd.ExecuteReader();
+                    int availableReg = 0;
+
+                    while (reader.Read())
+                    {
+                        if (reader.GetString(0).ToLower() == txtChangedInfo.Text.ToLower())
+                        {
+                            availableReg += 1;
+                        }
+                    }
+                    conn.Close();
+
+                    if (availableReg == 0)
+                    {
+                        changeRegistration = txtChangedInfo.Text;
+                        makeChange = true;
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("You already have a vehicle with that registration\nYou cannot have duplicates");
+                    }
+                }
+            }
+            else if(whatToChange == 2) //Rental cost part
+            {
+                if(txtChangedInfo.Text == "")
+                {
+                    MessageBox.Show("Please enter a new rental price or press cancel");
+                }
+                else
+                {
+                    try
+                    {
+                        changeRentalCost = Convert.ToDouble(txtChangedInfo.Text);
+                        makeChange = true;
+                        this.Close();
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Please ensure to enter a valid number");
+                    }
+                }
+            }
+            else if(whatToChange == 0)//closes the form for colour, thats it lol
+            {
+                if(cmbColour.SelectedItem == null)
+                {
+                    MessageBox.Show("Please select a new colour or press cancel");
+                }
+                else
+                {
+                    this.Close();
+                }
+            }
+        }
+    }
+}
---
+++ Pukki_Rental/Pukki_Rental/frmChangePopup.designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Pukki_Rental
+{
+    partial class frmChangePopup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblText = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.cmbColour = new System.Windows.Forms.ComboBox();
+            this.txtChangedInfo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblText
+            // 
+            this.lblText.AutoSize = true;
+            this.lblText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblText.Location = new System.Drawing.Point(8, 26);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(272, 20);
+            this.lblText.TabIndex = 0;
+            this.lblText.Text = "Please select the new colour below";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnCancel.Location = new System.Drawing.Point(12, 159);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(104, 42);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnSubmit.Location = new System.Drawing.Point(222, 159);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(104, 42);
+            this.btnSubmit.TabIndex = 2;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // cmbColour
+            // 
+            this.cmbColour.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbColour.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.cmbColour.FormattingEnabled = true;
+            this.cmbColour.Location = new System.Drawing.Point(12, 72);
+            this.cmbColour.Name = "cmbColour";
+            this.cmbColour.Size = new System.Drawing.Size(121, 24);
+            this.cmbColour.TabIndex = 14;
+            this.cmbColour.Visible = false;
+            this.cmbColour.SelectedIndexChanged += new System.EventHandler(this.cmbColour_SelectedIndexChanged);
+            // 
+            // txtChangedInfo
+            // 
+            this.txtChangedInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtChangedInfo.Location = new System.Drawing.Point(12, 72);
+            this.txtChangedInfo.Name = "txtChangedInfo";
+            this.txtChangedInfo.Size = new System.Drawing.Size(164, 26);
+            this.txtChangedInfo.TabIndex = 16;
+            this.txtChangedInfo.Visible = false;
+            // 
+            // frmChangePopup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(338, 213);
+            this.ControlBox = false;
+            this.Controls.Add(this.txtChangedInfo);
+            this.Controls.Add(this.cmbColour);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.lblText);
+            this.Name = "frmChangePopup";
+            this.Text = "Input the desired information";
+            this.Load += new System.EventHandler(this.frmChangePopup_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblText;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.ComboBox cmbColour;
+        private System.Windows.Forms.TextBox txtChangedInfo;
+    }
+}
